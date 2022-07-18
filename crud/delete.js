897bcd7b672a61757a2d86f92cbc29b6d1709cf9
“Satyam@141026” <satyam@141026@gmail.com>
2022-07-18 21:30:09 +0530

    add files

--- crud/delete.js
+++ crud/delete.js
@@ -3,7 +3,7 @@ console.log(dbConnect)
 
 async function deleteData(){
     const collection=await dbConnect();
-     let  data= await collection.deleteOne({name:"max 5"})
+     let  data= await collection.deleteMany({name:"max 7"})
       console.log(data)
 
 
